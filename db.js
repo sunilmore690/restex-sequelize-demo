ce9193117b753b3066be5778c0d7017d713bac8e
Sunil More <sunilmore@Sunils-MacBook-Air.local>
2022-06-05 00:16:12 +0530

    sequelize example for restex updated

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
 
 const Sequelize = require("sequelize");
 let sequelize = new Sequelize('database', 'user', 'password', {
-    dialect: 'postgres',
+    dialect: 'mysql',
     host: 'localhost',
-    port: 5432
+    port: 3306
   });
   
   sequelize
-          .authenticate()
-          .then(() => {
-              console.log('Connection has been established successfully.');
-          })
-          .catch((err) => {
-              console.log('Unable to connect to the database:', err);
-          });
-          sequelize.sync()
+    .authenticate()
+    .sync()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+        console.log('Unable to connect to the database:', err);
+    });
 module.exports = sequelize
